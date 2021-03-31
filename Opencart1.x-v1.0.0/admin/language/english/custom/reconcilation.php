8e0d90765cfd8acd5d41c2c5a488ab67d02a73b7
girkarjai <81283360+girkarjai@users.noreply.github.com>
2021-03-31 11:29:01 +0530

    Delete Opencart1.x-v1.0.0 directory

--- Opencart1.x-v1.0.0/admin/language/english/custom/reconcilation.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']           = 'Reconciliation';
-
-// Text
-$_['order_title']           = 'Orders';
-$_['text_custom_block']       = 'Custom Block';
-$_['text_total_informations'] = 'Total informations:';
